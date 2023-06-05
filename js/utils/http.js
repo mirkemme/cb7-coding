12fe14dcba811f273ba42b0d33812a0f78b296a3
Mirko <131855881+mirkemme@users.noreply.github.com>
2023-06-05 18:17:50 +0200

    Delete js directory

--- js/utils/http.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://jsonplaceholder.typicode.com/todos";
-
-export const GET = async (endpoint) => {
-    const res = await fetch(`${BASE_URL}${endpoint}`);
-    const data = await res.json();
-
-    return data;
-}
