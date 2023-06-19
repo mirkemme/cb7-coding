46c675ac1c3cdef93fdd44dc09da3eedef630131
Mirko Montaina <131855881+mirkemme@users.noreply.github.com>
2023-06-19 18:03:02 +0200

    Delete src directory

--- src/components/posts/Posts.jsx
+++
@@ -1,9 +0,0 @@
-import "./Posts.css"
-
-const Posts = () => { 
-    return (
-        <div className="Posts">Posts</div>
-    )
-}
-
-export default Posts;
--- src/icons/index.jsx
+++
@@ -1,7 +0,0 @@
-import CameraIcon from "./CameraIcon";
-import IGTVIcon from "./IGTVIcon";
-import LikeIcon from "./LikeIcon";
-import LogoIcon from "./LogoIcon";
-import MessangerIcon from "./MessangerIcon";
-
-export { CameraIcon, IGTVIcon, LikeIcon, LogoIcon, MessangerIcon };
